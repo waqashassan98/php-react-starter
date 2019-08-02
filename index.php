a454d8c3a5ac194dac510612faf9893163a3b7ed
Waqas Hassan <waqashassan98@users.noreply.github.com>
2019-08-02 17:08:54 +0500

    React PHP Starter

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+// https://davisonpro.dev/an-advanced-guide-on-how-to-setup-a-react-and-php/
+
+$user = (object) [
+    'name' => 'Jane Doe',
+    'email' => 'janedoe@gmail.com',
+    'logged' => true
+];
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>React PHP starter Kit</title>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/app/assets/css/app.css" type="text/css">
+    </head>
+    <script type="text/javascript">
+        var STATIC_URL = 'http://dev.react.com';
+        var myApp = {
+            user : <?php echo json_encode($user); ?>,
+            logged : <?php echo $user->logged; ?>
+        };
+    </script>
+    <body>
+
+        <div id="app"></div>
+
+        <script type="text/javascript" src="/app/assets/bundle/main.bundle.js" ></script>
+
+    </body>
+</html>
